a85f7d5e04ad0c0fbb2c436388e74020fd4b12d2
doudouwang32 <doudouwang32@gmail.com>
2026-07-15 20:28:21 +0800

    asdf

--- eval/eval.py
+++ eval/eval.py
@@ -11,7 +11,8 @@ from template_accuracy import Template_Accuracy
 # file_path = "../result/deepstl_with_ast_deepseek_v4_pro_result.txt"
 # file_path = "../result/deepstl_with_ast_operatorExplain_deepseek_v4_pro_result.txt"
 # file_path = "../tmp/data.txt"
-file_path = "../result/deepstl_with_ast_template_operator_knowledge_deepseek_v4_pro_result.txt"
+# file_path = "../result/deepstl_with_ast_template_operator_knowledge_deepseek_v4_pro_result.txt"
+file_path = "../result/two_way_iteration_deepseek_v4_pro_result.txt"
 
 if __name__ == "__main__":
     Exact_formula_match_accuracy = Exact_Formula_Match(file_path)
